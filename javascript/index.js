2637486b75f7e3b219034a2bc20d3d0833e26e5f
krishna singh <0.krishna1120@gmail.com>
2026-01-24 09:03:17 +0530

    Add initial JavaScript file with commented-out functions and loops

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+// function isLegal(age){
+//     if(age >= 18){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// let legalStatus1 = isLegal(20);
+// let legalStatus2 = isLegal(16);
+// console.log(legalStatus1); // true
+// console.log(legalStatus2); // false
+
+// for(let i = 0;i<10; i  ++){
+//     console.log(i+1);
+
