1881ce2e7ba26ad7c20c48cb2a99e8a802d36b7e
krishna singh <0.krishna1120@gmail.com>
2026-01-24 15:32:56 +0530

    Implement even number filtering function and log results

--- javascript/index.js
+++ javascript/index.js
@@ -12,4 +12,24 @@
 
 // for(let i = 0;i<10; i  ++){
 //     console.log(i+1);
+// }
 
+
+// const user = {
+//     name : "Alice",
+//     age : 25,
+//     gender : "female"
+// }
+// console.log(`Hello ${user.gender === "female" ? "Ms." : "Mr."} ${user.name} your age is ${user.age} you are ${user.age >= 18 ? "legal" : "not legal"} to vote.`);
+// function evenArray(nums){
+//     let evens = [];
+//     for(let i = 0; i < nums.length; i++){
+//         if(nums[i] % 2 === 0){
+//             evens.push(nums[i]);
+//         }
+//     }
+//     return evens;
+// }
+// const nums = [1,2,3,4,5,6,7,8,9,10];
+// const evenNums = evenArray(nums);
+// console.log(evenNums);
